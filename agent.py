6050e8f51f7cc7b4c4d05555f1cd2fc6b95c5932
Thomas <thomaschou861205@gmail.com>
2019-06-07 23:52:25 +0800

    so frustrating

--- agent.py
+++ agent.py
@@ -13,48 +13,33 @@ from collections import defaultdict
 class Tabular_Q_learning():
 	def __init__(self, env):
 		self.env = env
-		self.alpha = 0.7
-		self.gamma = 1
+		self.alpha = 0.3
+		self.gamma = 0.95
 		self.epsilon = 0.5
-		self.epsilon_decay = 0.999
+		self.epsilon_decay = 0.99
 		self.epsilon_min = 0.0
 
-		# print(env.observation_space.high)
-		# print(env.observation_space.low)
-		# self.cart_pos_bin = np.linspace(-2.4, 2.4, num=13)[1:-1]
-		# self.cart_vel_bin = np.linspace(-4, 4, num=15)[1:-1]
-		# self.pole_ang_bin = np.linspace(-0.42, -0.42, num=15)[1:-1]
-		# self.pole_vel_bin = np.linspace(-4, 4, num=13)[1:-1]
-		self.cart_pos_bin = np.linspace(-2.4, 2.4, num=3)[1:-1]
-		self.cart_vel_bin = np.linspace(-4, 4, num=3)[1:-1]
-		self.pole_ang_bin = np.linspace(-0.42, -0.42, num=9)[1:-1]
-		self.pole_vel_bin = np.linspace(-3, 3, num=5)[1:-1]
 
-		self.max_cart_vel = -100
-		self.min_cart_vel = 100
-		self.max_pole_vel = -100
-		self.min_pole_vel = 100
+		# self.cart_pos_bin = np.linspace(-2.4, 2.4, num=5)[1:-1]
+		self.cart_pos_bin = np.array([-2.0, 2.0])
+		# self.cart_vel_bin = np.linspace(-0.5, 0.5, num=5)[1:-1]
+		self.cart_vel_bin = np.array([-0.5, 0.5])
+		self.pole_ang_bin = np.linspace(-0.1, -0.1, num=11)[1:-1]
+		self.pole_vel_bin = np.linspace(-0.5, 0.5, num=5)[1:-1]
 
 		self.Q = defaultdict(lambda: 0*np.ones(env.action_space.n))
 
 	def state_coding(self, state):
-		# if state[1] > self.max_cart_vel:
-		# 	self.max_cart_vel = state[1]
-		# if state[1] < self.min_cart_vel:
-		# 	self.min_cart_vel = state[1]
-		# if state[3] > self.max_pole_vel:
-		# 	self.max_pole_vel = state[3]
-		# if state[3] < self.min_pole_vel:
-		# 	self.min_pole_vel = state[3]
-
 		cart_pos = np.digitize(state[0], self.cart_pos_bin)
 		cart_vel = np.digitize(state[1], self.cart_vel_bin)
 		pole_ang = np.digitize(state[2], self.pole_ang_bin)
 		pole_vel = np.digitize(state[3], self.pole_vel_bin)
 		
-		ret = (np.asscalar(cart_pos), np.asscalar(cart_vel), np.asscalar(pole_ang), np.asscalar(pole_vel))
-
+		# ret = (np.asscalar(cart_pos), np.asscalar(cart_vel), np.asscalar(pole_ang), np.asscalar(pole_vel))
+		ret = (np.asscalar(cart_pos), 0, np.asscalar(pole_ang), np.asscalar(pole_vel))
+		# ret = (0, 0, np.asscalar(pole_ang), np.asscalar(pole_vel))
 		return ret
+
 
 	def update_Q(self, state, action, reward, state_next, done):
 		coded_state = self.state_coding(state)
@@ -66,12 +51,13 @@ class Tabular_Q_learning():
 			target = reward + self.gamma * max(self.Q[coded_state_next])
 		self.Q[coded_state][action] += self.alpha * (target - self.Q[coded_state][action])
 
+
 	def act(self, state):
 		coded_state = self.state_coding(state)
-		if self.epsilon > self.epsilon_min:
-			self.epsilon *= self.epsilon_decay 
 
 		if np.random.rand() < self.epsilon:
+			if self.epsilon > self.epsilon_min:
+				self.epsilon *= self.epsilon_decay 
 			action = self.env.action_space.sample()
 		else:
 			action = np.argmax(self.Q[coded_state])
--- sample.py
+++ sample.py
@@ -5,27 +5,36 @@ from agent import Tabular_Q_learning, Policy_gradient, Actor_critics
 from copy import deepcopy
 
 
-episodes = 5000
+episodes = 10000
 reward_hist = np.zeros((episodes))
 
 env = gym.make('CartPole-v1')
-
 agent = Tabular_Q_learning(env)
+
 for episode in range(episodes):
+	if episode % 1000 == 0:
+		render = True
+	else:
+		render = False
+
 	state = env.reset()
 	for step in range(1000):
-	    # env.render()
+	    if render:
+	    	env.render()
+	    	# print(state)
+	    	# print(agent.state_coding(state))
 	    action = agent.act(state)
 	    state_next, reward, done, info = env.step(action) # take a random action
 	    agent.update_Q(state, action, reward, state_next, done)
-	    state = deepcopy(state_next)
+	    state = state_next
 
 	    if done:
 	    	reward_hist[episode] = step
+	    	env.close()
 	    	break
+
 	if episode % 100 == 0:
 		print('Episode {}/{} Score: {} Epsilon:{:.5f}'.format(episode, episodes, step, agent.epsilon))
-env.close()
 # print(agent.max_cart_vel, agent.min_cart_vel)
 # print(agent.max_pole_vel, agent.min_pole_vel)
 
