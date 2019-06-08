317478546e51ab3190332b68dc8cbbb1ccac228b
Thomas <thomaschou861205@gmail.com>
2019-06-09 05:12:13 +0800

    half descent

--- agent.py
+++ agent.py
@@ -13,51 +13,41 @@ from collections import defaultdict
 class Tabular_Q_learning():
 	def __init__(self, env):
 		self.env = env
-		self.alpha = 0.3
+		self.alpha = 0.05
 		self.gamma = 0.95
 		self.epsilon = 0.5
 		self.epsilon_decay = 0.99
 		self.epsilon_min = 0.0
 
 
-		# self.cart_pos_bin = np.linspace(-2.4, 2.4, num=5)[1:-1]
-		self.cart_pos_bin = np.array([-2.0, 2.0])
-		# self.cart_vel_bin = np.linspace(-0.5, 0.5, num=5)[1:-1]
-		self.cart_vel_bin = np.array([-0.5, 0.5])
-		self.pole_ang_bin = np.linspace(-0.1, -0.1, num=11)[1:-1]
-		self.pole_vel_bin = np.linspace(-0.5, 0.5, num=5)[1:-1]
-
-		self.Q = defaultdict(lambda: 0*np.ones(env.action_space.n))
+		self.cart_pos_bin = np.linspace(-2.4, 2.4, num=8)[1:-1]
+		self.cart_vel_bin = np.linspace(-3, 3, num=8)[1:-1]
+		self.pole_ang_bin = np.linspace(-0.42, 0.42, num=8)[1:-1]
+		self.pole_vel_bin = np.linspace(-2.0, 2.0, num=8)[1:-1]
+		self.Q = defaultdict(lambda: np.zeros(env.action_space.n))
 
 	def state_coding(self, state):
-		cart_pos = np.digitize(state[0], self.cart_pos_bin)
-		cart_vel = np.digitize(state[1], self.cart_vel_bin)
-		pole_ang = np.digitize(state[2], self.pole_ang_bin)
-		pole_vel = np.digitize(state[3], self.pole_vel_bin)
+		cart_pos = np.digitize([state[0]], self.cart_pos_bin)[0]
+		cart_vel = np.digitize([state[1]], self.cart_vel_bin)[0]
+		pole_ang = np.digitize([state[2]], self.pole_ang_bin)[0]
+		pole_vel = np.digitize([state[3]], self.pole_vel_bin)[0]
 		
-		# ret = (np.asscalar(cart_pos), np.asscalar(cart_vel), np.asscalar(pole_ang), np.asscalar(pole_vel))
-		ret = (np.asscalar(cart_pos), 0, np.asscalar(pole_ang), np.asscalar(pole_vel))
-		# ret = (0, 0, np.asscalar(pole_ang), np.asscalar(pole_vel))
-		return ret
+		
+		return (cart_pos, cart_vel, pole_ang, pole_vel)
 
 
 	def update_Q(self, state, action, reward, state_next, done):
 		coded_state = self.state_coding(state)
 		coded_state_next = self.state_coding(state_next)
 
-		if done:
-			target = reward
-		else:
-			target = reward + self.gamma * max(self.Q[coded_state_next])
+		target = reward + self.gamma * max(self.Q[coded_state_next])
 		self.Q[coded_state][action] += self.alpha * (target - self.Q[coded_state][action])
 
 
 	def act(self, state):
 		coded_state = self.state_coding(state)
 
-		if np.random.rand() < self.epsilon:
-			if self.epsilon > self.epsilon_min:
-				self.epsilon *= self.epsilon_decay 
+		if np.random.uniform(0, 1) < self.epsilon:
 			action = self.env.action_space.sample()
 		else:
 			action = np.argmax(self.Q[coded_state])
--- sample.py
+++ sample.py
@@ -5,26 +5,30 @@ from agent import Tabular_Q_learning, Policy_gradient, Actor_critics
 from copy import deepcopy
 
 
-episodes = 10000
+episodes = 1000
 reward_hist = np.zeros((episodes))
 
 env = gym.make('CartPole-v1')
 agent = Tabular_Q_learning(env)
 
 for episode in range(episodes):
-	if episode % 1000 == 0:
+	if episode == episodes-1:
 		render = True
 	else:
 		render = False
 
 	state = env.reset()
-	for step in range(1000):
+	agent.epsilon *= agent.epsilon_decay
+	for step in range(500):
 	    if render:
 	    	env.render()
 	    	# print(state)
 	    	# print(agent.state_coding(state))
+	    	pass
 	    action = agent.act(state)
 	    state_next, reward, done, info = env.step(action) # take a random action
+	    if done and step < 499:
+	    	reward = -5000
 	    agent.update_Q(state, action, reward, state_next, done)
 	    state = state_next
 
@@ -33,10 +37,9 @@ for episode in range(episodes):
 	    	env.close()
 	    	break
 
-	if episode % 100 == 0:
+	if episode % 1 == 0:
 		print('Episode {}/{} Score: {} Epsilon:{:.5f}'.format(episode, episodes, step, agent.epsilon))
-# print(agent.max_cart_vel, agent.min_cart_vel)
-# print(agent.max_pole_vel, agent.min_pole_vel)
+
 
 print('avg. {}'.format(np.mean(reward_hist)))
 print('last 100 {}'.format(np.mean(reward_hist[-100:])))
