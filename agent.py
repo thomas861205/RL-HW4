6d490e1c0883b5294b8611568f69260cc425c706
Thomas <thomaschou861205@gmail.com>
2019-06-10 03:05:30 +0800

    it's running

--- agent.py
+++ agent.py
@@ -1,5 +1,11 @@
 import numpy as np
 from collections import defaultdict
+import keras.layers as layers
+from keras.models import Model
+from keras.optimizers import Adam
+import keras.backend as K
+from keras.initializers import glorot_uniform
+
 
 # Observation: 
 #     Type: Box(4)
@@ -54,11 +60,87 @@ class Tabular_Q_learning():
 		return action
 
 
+
 class Policy_gradient():
-	def __init__(self):
-		pass
+	def __init__(self, env):
+		self.env = env
+		self.num_actions = env.action_space.n
+		self.hidden_layer_neurons = 8
+		self.gamma = .99
+		self.dimen = len(env.reset())
+		self.print_every = 100
+		self.batch_size = 50
+		self.num_episodes = 10000
+		self.render = False
+		self.lr = 1e-2
+		self.goal = 190
+		self.build_model()
+		# print(self.model_predict.summary())
+
+
+	def build_model(self):
+	    x = layers.Input(shape=self.env.reset().shape, name="x")
+	    self.adv = layers.Input(shape=[1], name="advantages")
+	    h1 = layers.Dense(self.hidden_layer_neurons, 
+	                     activation="relu", 
+	                     use_bias=False,
+	                     kernel_initializer=glorot_uniform(seed=42),
+	                     name="hidden_1")(x)
+	    out = layers.Dense(self.env.action_space.n, 
+	                       activation="softmax", 
+	                       kernel_initializer=glorot_uniform(seed=42),
+	                       use_bias=False,
+	                       name="out")(h1)
+
+
+	    self.model_train = Model(inputs=[x, self.adv], outputs=out)
+	    self.model_train.compile(loss=self.custom_loss, optimizer=Adam(self.lr))
+	    self.model_predict = Model(inputs=[x], outputs=out)
+
+
+	def custom_loss(self, y_true, y_pred):
+	    log_lik = K.log(y_true * (y_true - y_pred) + (1 - y_true) * (y_true + y_pred))
+	    return K.mean(log_lik * self.adv, keepdims=True)
+
+
+	def discount_rewards(self, r, gamma=0.99):
+	    prior = 0
+	    out = []
+	    for val in r:
+	        new_val = val + prior * gamma
+	        out.append(new_val)
+	        prior = new_val
+	    return np.array(list(reversed(out)))
+
+
+	def score_model(self, model, num_tests, render=False):
+	    scores = []    
+	    for num_test in range(num_tests):
+	        observation = self.env.reset()
+	        reward_sum = 0
+	        while True:
+	            if render:
+	                self.env.render()
+
+	            state = np.reshape(observation, [1, self.dimen])
+	            predict = model.predict([state])[0]
+	            action = np.argmax(predict)
+	            observation, reward, done, _ = self.env.step(action)
+	            reward_sum += reward
+	            if done:
+	                break
+	        scores.append(reward_sum)
+	    self.env.close()
+	    return np.mean(scores)
 
 
 class Actor_critics():
 	def __init__(self):
 		pass
+
+if __name__ == '__main__':
+	import gym
+
+	env = gym.make('CartPole-v1')
+	agent = Policy_gradient(env)
+	agent.run()
--- sample.py
+++ sample.py
@@ -5,47 +5,120 @@ from agent import Tabular_Q_learning, Policy_gradient, Actor_critics
 from copy import deepcopy
 
 
-episodes = 1000
-reward_hist = np.zeros((episodes))
+def agent_Q():
+	episodes = 1000
+	reward_hist = np.zeros((episodes))
 
-env = gym.make('CartPole-v1')
-agent = Tabular_Q_learning(env)
+	env = gym.make('CartPole-v1')
+	agent = Tabular_Q_learning(env)
 
-for episode in range(episodes):
-	if episode == episodes-1:
-		render = True
-	else:
-		render = False
+	for episode in range(episodes):
+		if episode == episodes-1:
+			render = True
+		else:
+			render = False
 
-	state = env.reset()
-	agent.epsilon *= agent.epsilon_decay
-	for step in range(500):
-	    if render:
-	    	# env.render()
-	    	# print(state)
-	    	# print(agent.state_coding(state))
-	    	pass
-	    action = agent.act(state)
-	    state_next, reward, done, info = env.step(action) # take a random action
-	    if done and step < 499:
-	    	reward = -1e5
-	    agent.update_Q(state, action, reward, state_next, done)
-	    state = state_next
+		state = env.reset()
+		agent.epsilon *= agent.epsilon_decay
+		for step in range(500):
+		    if render:
+		    	# env.render()
+		    	pass
+		    action = agent.act(state)
+		    state_next, reward, done, info = env.step(action) # take a random action
+		    if done and step < 499:
+		    	reward = -1e5
+		    agent.update_Q(state, action, reward, state_next, done)
+		    state = state_next
 
-	    if done:
-	    	reward_hist[episode] = step
-	    	env.close()
-	    	break
+		    if done:
+		    	reward_hist[episode] = step
+		    	env.close()
+		    	break
 
-	if episode % 1 == 0:
-		print('Episode {}/{} Score: {} Epsilon:{:.5f}'.format(episode, episodes, step, agent.epsilon))
-
-
-print('avg. {}'.format(np.mean(reward_hist)))
-print('last 100 {}'.format(np.mean(reward_hist[-100:])))
-print('peak {}'.format(np.max(reward_hist)))
-
-plt.plot(reward_hist)
-plt.show()
+		if episode % 1 == 0:
+			print('Episode {}/{} Score: {} Epsilon:{:.5f}'.format(episode, episodes, step, agent.epsilon))
 
 
+	print('avg. {}'.format(np.mean(reward_hist)))
+	print('last 100 {}'.format(np.mean(reward_hist[-100:])))
+	print('peak {}'.format(np.max(reward_hist)))
+
+	plt.plot(reward_hist)
+	plt.show()
+
+
+
+def agent_pg():
+	episodes = 1000
+	batch_size = 50
+	episode = 0
+	steps = 0
+	losses = []
+	reward_sum = []
+	test_score = []
+
+	env = gym.make('CartPole-v1')
+	agent = Policy_gradient(env)
+
+	states = np.empty(0).reshape(0,agent.dimen)
+	actions = np.empty(0).reshape(0,1)
+	rewards = np.empty(0).reshape(0,1)
+	discounted_rewards = np.empty(0).reshape(0,1)
+
+
+
+	while episode < episodes:
+		observation = agent.env.reset()
+		for step in range(500):
+		    state = np.reshape(observation, [1, agent.dimen])
+		    
+		    predict = agent.model_predict.predict([state])[0]
+		    action = np.random.choice(range(agent.num_actions),p=predict)
+		    
+		    states = np.vstack([states, state])
+		    actions = np.vstack([actions, action])
+		    
+		    observation, reward, done, _ = agent.env.step(action)
+		    rewards = np.vstack([rewards, reward])
+		    
+		    if done:
+		        reward_sum.append(step)
+		        discounted_rewards_episode = agent.discount_rewards(rewards, agent.gamma)       
+		        discounted_rewards = np.vstack([discounted_rewards, discounted_rewards_episode])
+		        
+		        rewards = np.empty(0).reshape(0,1)
+
+		        if (episode + 1) % batch_size == 0:
+		            discounted_rewards = (discounted_rewards - discounted_rewards.mean()) / discounted_rewards.std()
+		            discounted_rewards = discounted_rewards.squeeze()
+		            actions = actions.squeeze().astype(int)
+		           
+		            actions_train = np.zeros([len(actions), agent.num_actions])
+		            actions_train[np.arange(len(actions)), actions] = 1
+		            
+		            loss = agent.model_train.train_on_batch([states, discounted_rewards], actions_train)
+		            losses.append(loss)
+
+		            states = np.empty(0).reshape(0,agent.dimen)
+		            actions = np.empty(0).reshape(0,1)
+		            discounted_rewards = np.empty(0).reshape(0,1)
+
+
+		        score = agent.score_model(agent.model_predict,1)
+		        test_score.append(score)
+		        every = 100
+		        if (episode + 1) % every == 0:
+		            print("Avg steps for episode {}/{}: {:0.2f} Test Score: {:0.2f} Loss: {:0.6f} ".format(
+		                (episode + 1), episodes, sum(reward_sum[-every:])/every, 
+		                test_score[-1], np.mean(losses[-every:])))
+        
+		        episode += 1
+		        break
+	plt.plot(test_score)
+	plt.show()
+
+
+if __name__ == '__main__':
+	# agent_Q()
+	agent_pg()
